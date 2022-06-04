a43ff24374b5cb360aa6bb4ebb219a395128dc2e
KeshavPratap-K <keshavpratapofcl@gmail.com>
2022-06-05 00:19:54 +0530

    Angular Migration completed Home, Projects and about

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,10 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -26,6 +30,10 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'projects', component: ProjectsComponent},
+      { path: 'about', component: AboutComponent},
+      { path: 'contact', component: ContactComponent},
+      { path: 'resume', component: ResumeComponent},
     ])
   ],
   providers: [],
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Keshav-Dev
+<div align="center">
+</div>
+<h1 align="center">
+  Keshav-Dev -v1
+</h1>
+
+## 🚨 Forking this repo (please read!)
+
+Many people have contacted me asking me if they can use this code for their own website, and the answer to that question is usually **yes, with attribution**.
+
+I value keeping my site open source, but as you all know, _**plagiarism is bad**_. It's always disheartening whenever I find that someone has copied my site without giving me credit. I spent a non-trivial amount of effort building and designing this iteration of my website, and I am proud of it! All I ask of you all is to not claim this effort as your own.
+
+### TL;DR
+
+Yes, you can fork this repo. Please give me proper credit by linking back to [keshav-dev.herokuapp.com](https://keshav-dev.herokuapp.com). Thanks!
